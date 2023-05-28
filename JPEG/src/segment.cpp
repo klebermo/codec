86c071b8d02718d47215002715e7916aa00fe987
Kleber Mota de Oliveira <klebermo@gmail.com>
2023-05-28 08:48:56 -0300

    Update 28/05/2023 08:48

--- JPEG/src/segment.cpp
+++ JPEG/src/segment.cpp
@@ -7,27 +7,55 @@ Segment::Segment(unsigned char marker[2], unsigned char length[2])  {
         this->length[1] = length[1];
 };
 
-Segment::segment(std::initializer_list<unsigned char> marker, std::initializer_list<unsigned char> length) {
-    this->marker[0] = marker[0];
-    this->marker[1] = marker[1];
-    this->length[0] = length[0];
-    this->length[1] = length[1];
+Segment::Segment(std::initializer_list<unsigned char> marker_list, std::initializer_list<unsigned char> length_list) {
+    int m_size = marker_list.size();
+    int index = 0;
+    for(auto it : marker_list) {
+        unsigned char data = (unsigned char)it;
+        this->marker[index++] = data;
+    }
+
+    int l_size = length_list.size();
+    index = 0;
+    for(auto it : length_list) {
+        unsigned char data = (unsigned char)it;
+        this->length[index++] = data;
+    }
 }
 
 Segment::Segment(unsigned char marker[2], int length) {
     this->marker[0] = marker[0];
     this->marker[1] = marker[1];
-    this->length = intToUnsignedCharArr(length);
+
+    unsigned char length_str[2];
+    intToUnsignedCharArr(length, length_str);
+
+    this->length[0] = length_str[0];
+    this->length[1] = length_str[1];
 }
 
-Segment::Segment(std::initializer_list<unsigned char> marker, int length) {
-    this->marker[0] = marker[0];
-    this->marker[1] = marker[1];
-    this->length = intToUnsignedCharArr(length);
+Segment::Segment(std::initializer_list<unsigned char> marker_list, int length) {
+    int m_size = marker_list.size();
+    int index = 0;
+    for(auto it : marker_list) {
+        unsigned char data = (unsigned char)it;
+        this->marker[index++] = data;
+    }
+
+    unsigned char length_str[2];
+    intToUnsignedCharArr(length, length_str);
+
+    this->length[0] = length_str[0];
+    this->length[1] = length_str[1];
 }
 
 unsigned char * Segment::getMarker() {
     return marker;
+}
+
+void Segment::setMarker(unsigned char marker[2]) {
+    this->marker[0] = marker[0];
+    this->marker[1] = marker[1];
 }
 
 unsigned char * Segment::getLength() {
--- JPEG/src/segment.hpp
+++ JPEG/src/segment.hpp
@@ -9,21 +9,17 @@ protected:
     unsigned char length[2];
 public:
     Segment(unsigned char marker[2], unsigned char length[2]);
-
     Segment(std::initializer_list<unsigned char> marker, std::initializer_list<unsigned char> length);
-
     Segment(unsigned char marker[2], int length);
-
     Segment(std::initializer_list<unsigned char> marker, int length);
 
     unsigned char * getMarker();
+    void setMarker(unsigned char marker[2]);
 
     unsigned char * getLength();
-
     void setLength(unsigned char length[2]);
 
     virtual void setData(unsigned char * data, int data_length) = 0;
-
     void intToUnsignedCharArr(int value, unsigned char result[2]);
 };
 
